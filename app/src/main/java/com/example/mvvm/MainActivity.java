f76064ad41e1d301ca2b48ba5ec7dac732fca488
ybdl <komeyang@drsvip.com>
2022-01-13 11:20:32 +0800

    first

--- app/src/main/java/com/example/mvvm/MainActivity.java
+++ app/src/main/java/com/example/mvvm/MainActivity.java
@@ -6,6 +6,9 @@ import androidx.databinding.ViewDataBinding;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.Editable;
+import android.text.TextWatcher;
 
 import com.example.mvvm.databinding.ActivityMainBinding;
 
@@ -15,10 +18,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main);
+        String a = "1111";
         NumViewModel numViewModel = new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory()).get(NumViewModel.class);
         ActivityMainBinding mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         mainBinding.setVm(numViewModel);
         mainBinding.setLifecycleOwner(this);
+        mainBinding.textView2.setText(a);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mainBinding.textView2.setText("y1y11y781y");
+                mainBinding.edit.setText("这是后来的edittext");
+            }
+        }, 3000);
+        mainBinding.edit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+//                numViewModel.editLiveData.setValue(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
 
     }
--- app/src/main/java/com/example/mvvm/NumViewModel.java
+++ app/src/main/java/com/example/mvvm/NumViewModel.java
@@ -14,6 +14,7 @@ import androidx.lifecycle.ViewModel;
 public class NumViewModel extends ViewModel {
     private int num = 0;
     public MutableLiveData<String> mutableLiveData;
+    public MutableLiveData<String> editLiveData;
     public MutableLiveData<String> imageLiveData;
 
 
@@ -23,6 +24,8 @@ public class NumViewModel extends ViewModel {
         imageLiveData = new MutableLiveData<>();
 //        imageLiveData.setValue("https://dss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3248357717,1973320706&fm=26&gp=0.jpg");
         imageLiveData.setValue("https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2F1113%2F033120123529%2F200331123529-4-1200.jpg&refer=http%3A%2F%2Fimg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1639647580&t=ed5dc8a70c580071b35c6407fbc9c8ce");
+        editLiveData = new MutableLiveData<>();
+        editLiveData.setValue("jjjjj");
     }
 
     public void plusNum(){
